bde9d8cd140c6235e0c5450c27b7924b507f230e
xuyinghan <188655147@qq.com>
2017-11-09 10:18:22 +0800

    ddd

--- src/main/java/com/home/test/TestAdd.java
+++ src/main/java/com/home/test/TestAdd.java
@@ -6,10 +6,10 @@ import com.home.vo.User;
 public class TestAdd {
     public static void main(String[] args) {
         User vo = new User();
-        vo.setUserno("11112236");
+        vo.setUserno("11112246");
         vo.setPassword("123123123");
         vo.setName("王者");
-        vo.setId(26);
+        vo.setId(27);
         try{
             System.out.println(ServiceFactory.getIUserServiceInstance().insert(vo));
         } catch (Exception e) {
