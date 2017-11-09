e3096f0ddd350c84e756b8e2974d6d6c360ef4a5
xuyinghan <188655147@qq.com>
2017-11-09 10:09:13 +0800

    ddd

--- src/main/java/com/home/factory/DAOFactory.java
+++ src/main/java/com/home/factory/DAOFactory.java
@@ -1,12 +1,18 @@
 package com.home.factory;
 
+import com.home.dao.IKongtiaoDAO;
 import com.home.dao.IUserDAO;
+import com.home.dao.Impl.KongtiaoDAOImpl;
 import com.home.dao.Impl.UserDAOImpl;
+import com.home.service.IKongtiaoService;
 
 import java.sql.Connection;
 
 public class DAOFactory {
     public static IUserDAO getIUserDAOInstance(Connection conn){
         return new UserDAOImpl(conn);
+    }
+    public static IKongtiaoDAO getIKongtiaoDAOInstance(Connection conn){
+        return new KongtiaoDAOImpl(conn);
     }
 }
